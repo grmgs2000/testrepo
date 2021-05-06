4914200dee483559d40704fad44522eaaff6091d
grmgs2000 <79459496+grmgs2000@users.noreply.github.com>
2021-05-06 18:06:48 -0400

    Add files via upload

---
+++ Sadguru.py
@@ -0,0 +1,11 @@
+import time
+
+
+user_input = input("Please Enter You toughts"  )
+Name_list = ["Sadguru", "Jim Twik", "David Goggins"]
+while user_input in Name_list:
+    print(100*("Organize Your" , "Mind" , "Body" , "&", "Emotion" " in One Direction"))
+    time.sleep(10)
+else :
+    print("Please Drift back to your Intellegence")
+    
